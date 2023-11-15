d5ba20914fc40a710ab268408cc24284552fc92e
Bethan Manley <46747172+bethanmanley@users.noreply.github.com>
2023-11-15 13:47:30 +0000

    Create file_renaming.py

---
+++ file_renaming.py
@@ -0,0 +1,62 @@
+import os
+import pandas as pd
+
+def rename_files(excel_file, directory):
+    # Get the current working directory
+    current_dir = os.getcwd()
+
+    # Create the full paths for the Excel file and the directory
+    excel_path = os.path.join(current_dir, excel_file)
+    directory_path = os.path.join(current_dir, directory)
+
+    # Load the Excel file
+    df = pd.read_excel(excel_path)
+
+    # Iterate through each row in the DataFrame
+    for index, row in df.iterrows():
+        sampleid = str(row['CCBB'])
+        name = str(row['Sample_Name'])
+
+        # Get a list of all file names in the directory
+        file_names = os.listdir(directory_path)
+
+        # Find the matching filenames based on the CCBB
+        matching_files = [filename for filename in file_names if filename.startswith(sampleid.split('id', 1)[0] + 'id')]
+
+        for matching_file in matching_files:
+            # Get the full path of the matching file
+            old_file_path = os.path.join(directory_path, matching_file)
+
+            # Extract the portion after '_S' from the matching file
+            extension = matching_file.split('_S')[-1]
+
+            # Replace 'RIBO' with 'SSU' in the 'Name' column
+            if 'RIBO' in name:
+                new_name = name.replace('RIBO', 'SSU')
+            else:
+                new_name = name
+
+            # Create the new file name based on the extracted extension, modified 'Name' column, and the 'CCBB' prefix
+            new_file_name = new_name + '_S' + extension
+
+            # Check for duplicate names
+            if new_file_name in file_names:
+                print(f"Duplicate file name: {new_file_name}. Skipping renaming for {matching_file}")
+            else:
+                # Get the full path of the new file name
+                new_file_path = os.path.join(directory_path, new_file_name)
+
+                # Rename the file
+                os.rename(old_file_path, new_file_path)
+                print(f"Renamed {matching_file} to {new_file_name}")
+
+        if not matching_files:
+            print(f"File with SampleID {sampleid} not found in the directory")
+
+
+
+# Example usage
+excel_file = 'Documents/Data/Sample_Key_Run1.csv'  # Provide the name of your Excel file
+directory = 'Data_processing/Utrecht/ITS2'  # Provide the path to the directory containing the files
+
+rename_files(excel_file, directory)
